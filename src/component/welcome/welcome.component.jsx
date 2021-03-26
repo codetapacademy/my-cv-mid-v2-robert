624b8c74ea4bb0c5a899a44ff5036d96458e00f0
Robert Alexandru <romaniarobertverde@yahoo.com>
2021-03-26 07:38:56 +0000

    set dynamic values and start to style the page

--- src/component/welcome/welcome.component.jsx
+++ src/component/welcome/welcome.component.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import * as WS from './welcome.styled'
 
 export const Welcome = () => {
+  const title = "Welcome to my Sanctuary"
+  const headerOne = "Robert Alexandru"
+  const position = "Junior Front End developer"
+  const about = "About me"
+  const description = "Coding flows through my veins and my goal is to continuously improve and make a positive impact in people’s life by delivering amazing front end experiences."
+  const downloadButton = "Download my Cv"
+
   return (
     <div>
-      <div>Welcome to my Sanctuary</div>
+      <WS.Title>{ title }</WS.Title>
       <button>YT</button>
-      <h1>Robert Alexandru</h1>
-      <h2>Junior Front End developer</h2>
-      <h3>About me</h3>
+      <h1>{ headerOne }</h1>
+      <h2>{ position }</h2>
+      <h3>{ about }</h3>
       <div>
-        <p>Coding flows through my veins and my goal is to continuously improve and make a positive impact in people’s life by delivering amazing front end experiences.</p>
+        <p>{ description }</p>
       </div>
-      <button>Download my Cv</button>
+      <button>{ downloadButton }</button>
       <br></br>
       <button>AD</button> 
     </div>
